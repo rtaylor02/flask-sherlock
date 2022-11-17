da2045dbbcfb2dba3aa07594ec3408ba9994b35f
rtaylor02 <97712087+rtaylor02@users.noreply.github.com>
2022-11-17 09:35:36 +0000

    Update run.py

--- app/run.py
+++ app/run.py
@@ -1,8 +1,38 @@
-from flask import Flask
+import pytest
 
-app = Flask(__name__)
+from run import app as application
 
 
-@app.route("/")
-def hello_world():
-    return "Hello, Flask!"
+@pytest.fixture()
+def app():
+    application.config.update({
+        "TESTING": True,
+    })
+    yield application
+
+
+@pytest.fixture
+def client(app):   
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
+
+
+def test_api(client):
+    response = client.get("/api/v1/movies/recommend")
+    assert response.status_code == 200
+    assert response.is_json
+    assert response.get_json()[0]["title"] != ""
+
+    response = client.get("/api/v1/movies/recommend?title=Kingpin")
+    assert response.status_code == 200
+    assert response.is_json
+    assert len(response.get_json()) >= 2
+
+    response = client.get("/api/v1/movies/recommend?title=Lost%20in%20Translation")
+    assert response.status_code == 200
+    assert response.is_json
+    assert len(response.get_json()) >= 5
